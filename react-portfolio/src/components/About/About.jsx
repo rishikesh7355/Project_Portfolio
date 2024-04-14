31972f4cba594cfe014b0f76f845947eeb166736
rishikesh7355 <rishiyadav280198@gmail.com>
2024-04-14 23:05:49 +0530

    about section styling

--- react-portfolio/src/components/About/About.jsx
+++ react-portfolio/src/components/About/About.jsx
@@ -1,13 +1,19 @@
 import {getImageUrl} from "../../utils"
+import styles from "./About.module.css"
 export const About = () => {
     return(
-       <section>
-            <h2>About</h2>
-            <div><img src={getImageUrl("about/aboutImage.png")} alt="sitting with laptop"/>
-                <ul>
-                    <li>
-                        <img src={getImageUrl("about/uiIcon.png")} alt="cursor icon" />
-                        <div>
+       <section className={styles.container} id="about">
+            <h2 className={styles.title}>About</h2>
+            <div className={styles.content}>
+                <img 
+                    src={getImageUrl("about/aboutImage.png")} 
+                    alt="sitting with laptop"
+                    className={styles.aboutImage}
+                    />
+                <ul className={styles.aboutItems}>
+                    <li className={styles.aboutItem}>
+                        <img src={getImageUrl("about/uiIcon.png")} alt="Ui icon" />
+                        <div className={styles.aboutItemText}>
                             <h3>Frontend Developer</h3>
                             <p>I'm a Frontend Developer with 2.5 years
                                 experience in building responsive sites, optimization,
@@ -15,22 +21,22 @@ export const About = () => {
                             </p>
                         </div>
                     </li>
-                    <li>
+                    <li className={styles.aboutItem}>
                         <img src={getImageUrl("about/cursorIcon.png")} alt="server icon" />
-                        <div>
+                        <div className={styles.aboutItemText}>
                             <h3>FullStack Developer</h3>
                             <p>I have also knowledge of MERN stack and practiced 
                                 experience in building responsive sites, optimization,
-                                and debugging suing MongoDB, ExpressJS, ReactJS and NodeJS
+                                and debugging using MongoDB, ExpressJS, ReactJS and NodeJS
                             </p>
                         </div>
                     </li>
-                    <li>
+                    <li className={styles.aboutItem}>
                         <img src={getImageUrl("about/serverIcon.png")} alt="server icon" />
-                        <div>
+                        <div className={styles.aboutItemText}>
                             <h3>Backend Developemt</h3>
                             <p>Beginner in Java + spring Boot  and 
-                                worked on beginner level tasks.Familiar with Eclipse ,SoapUI 
+                                worked on beginner level tasks.Familiar with Eclipse, SoapUI, etc. 
                             </p>
                         </div>
                     </li>
