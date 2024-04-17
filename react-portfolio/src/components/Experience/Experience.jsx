d5d38e5ce2e9609edf5d2d46beef3e03ab782bb8
rishikesh7355 <rishiyadav280198@gmail.com>
2024-04-17 10:59:37 +0530

    expereive section data

---
+++ react-portfolio/src/components/Experience/Experience.jsx
@@ -0,0 +1,46 @@
+import skills from "../../data/skills.json"
+import history from "../../data/history.json"
+import { getImageUrl } from "../../utils"
+import styles from "./Experience.module.css"
+
+export const Experience = () => {
+    return <section className={styles.container} id="experience">
+        <h2 className={styles.title}>Experience</h2>
+        <div className={styles.content}>
+            <div className={styles.skills}>
+                {skills.map((skill,id)=> {
+                return (
+                <div key={id} className={styles.skill}>
+                    <div className={styles.skillImgBox}>
+                        <img src={getImageUrl(skill.imageSrc)} alt={skill.title}></img>
+                     </div>
+                     <p>{skill.title}</p>
+                </div>
+                )
+            })}
+            </div>
+            <ul>
+                {
+                    history.map((item,id)=>{
+                        return (
+                            <li key={id}>
+                               <img 
+                                src={getImageUrl(item.imageSrc)} 
+                                alt={`${item.organisation} Logo`} />
+                                <div>
+                                    <h3>
+                                        {`${item.role}, ${item.organisation}`}
+                                    </h3>
+                                    <p>{`${item.startDate}, ${item.endDate}`}</p>
+                                    <ul>{item.experiences.map((exp,id) => {
+                                        return <li key={id}>{exp}</li>
+                                    })}</ul>
+                                </div>
+                            </li>
+                        )
+                    })
+                }
+            </ul>
+        </div>
+    </section>
+}
